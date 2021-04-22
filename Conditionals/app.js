6946f196e1fadda370420d54d45f67237f798954
samsul-rijal <samsulr99@gmail.com>
2021-04-23 00:14:46 +0700

    add loop

--- Conditionals/app.js
+++ Conditionals/app.js
@@ -11,6 +11,7 @@ document.write("Menggunakan perulangan for" + "<br>");
 for (let i = 1; i <= 10; i++) {
   document.write(i + " ");
 }
+//Output : 12345678910
 
 document.write("<br>" + "Menggunakan perulangan while" + "<br>");
 let i = 1;
@@ -18,6 +19,7 @@ while (i <= 10) {
   document.write(i + " ");
   i++;
 }
+//Output : 12345678910
 
 document.write("<br>" + "Menggunakan Reverse" + "<br>");
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -25,3 +27,22 @@ numbers.reverse();
 for (let i = 0; i < numbers.length; i++) {
   document.write(numbers[i] + " ");
 }
+//Output : 10,9,8,7,6,5,4,3,2,1
+
+
+for(let i = 20; i >= 1; i--) {
+console.log(i);
+}
+//Output : 10 9 8 7 6 5 4 3 2 1
+
+
+for (let i = 1; i <= 5; i++){
+console.log(i * i);
+}
+//output kuadrat sebanyak 5 kali:  1 4 9 16 25
+
+
+for (let i = 1; i <= 5; i++){
+console.log(i * 3);
+}
+//output kelipatan 3 sebanyak 5 kali: 3 6 9 12 15
