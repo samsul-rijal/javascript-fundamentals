d1c760fcc2a860162bf1711245778d4211d18f95
samsul-rijal <samsulr99@gmail.com>
2021-04-26 16:14:18 +0700

    add object

---
+++ Object/app.js
@@ -0,0 +1,12 @@
+const orang = {
+  nama: "Samsul Rijal",
+  alamat: "Rangkasbitung",
+  umur: 26,
+};
+
+// merubah data di dalam object
+orang.nama = "Saiful Bahri";
+orang.umur = 20;
+
+console.log(orang.nama); // hanya menampilkan nama
+console.log(orang); // menampilkan semua data yang ada dalam object orang
