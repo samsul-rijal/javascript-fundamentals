cc18e086ffaae161266ad6ffbfea2ca7d03224e1
samsul-rijal <samsulr99@gmail.com>
2021-04-26 15:57:43 +0700

    add array

---
+++ Array/app.js
@@ -0,0 +1,12 @@
+let names = ["Samsul", "Rijal", "Fahmi"];
+
+// push berfungsi untuk menambahkan data ke dalam array
+names.push("Rifky"); //menambahkan 1 data ke dalam array
+names.push("Joyo", "Jono", "Joko"); //menambahkan lebih dari 1 data ke dalam array
+
+// merubah data dalam array
+names[0] = "Rizky";
+names[1] = "Rahmat";
+
+console.log(names[2]); //menampilkan data array sesuai urutan index nya
+console.log(names);
